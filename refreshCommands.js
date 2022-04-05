220db2e5653148b1d562419e6f2b5bb13283a061
zoodogood <zoodogood@gmail.com>
2022-04-05 14:51:18 +0300

    Now i can register SlashCommands

---
+++ refreshCommands.js
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
+
+
+const commands = [{
+  name: 'help',
+  description: 'Have fun!'
+}];
+
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+
+(async () => {
+  try {
+    const clientId = (await rest.get( Routes.user() ))
+      .id;
+
+    console.info('----\nStarted refreshing application (/) commands.');
+
+    const out = await rest.put(
+      Routes.applicationCommands(clientId),
+      { body: commands },
+    );
+
+    console.log(out);
+    console.info('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
