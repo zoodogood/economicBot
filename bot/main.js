2c46cc8be7938b3d82fe24788d1e5cefb69fbd17
zoodogood <zoodogood@gmail.com>
2022-04-05 14:51:27 +0300

    minor

---
+++ bot/main.js
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import { Client, Intents } from 'discord.js';
+
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.id}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'help') {
+    await interaction.reply('Pong!');
+  }
+});
+
+export default client;
+client.login(process.env.TOKEN);
--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-import 'dotenv/config';
-console.log(process.env);
+console.clear();
+
+
+class App {
+
+  async load(){
+    // bot
+    const client = await import('./bot/main.js');
+    this.client = client;
+  }
+}
+
+globalThis.app = new App();
+app.load();
