cc0353ebd988e8e72a7672d2ed2decc73b67b261
zoodogood <zoodogood@gmail.com>
2022-04-16 11:47:40 +0300

    none changes

--- database/Database/index.js
+++ database/Database/index.js
@@ -4,9 +4,6 @@ class Database {
   constructor({ host, user, password, database }){
     this.sql = sql;
     this.pool = this.sql.createPool({ host, user, password, database }).promise();
-    this.pool.execute("SELECT * FROM `global` WHERE `id` = 1").then(([rows]) => {
-      console.log( rows.at(0) );
-    });
   }
 }
 
