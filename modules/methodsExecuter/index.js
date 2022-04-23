4f9e687c6d207d6addc0d702ae3eb281f8a675fe
zoodogood <zoodogood@gmail.com>
2022-04-23 12:21:39 +0300

    new MethodsExecuter().execute("command_help_MainButton", ...args); it's apply globalThis.commands.get("help").onMainButton.apply(<this>, args);

---
+++ modules/methodsExecuter/index.js
@@ -0,0 +1,42 @@
+/***
+  @useExample
+  new MethodsExecuter().execute("command_help_MainButton", ...args);
+  it's apply globalThis.commands.get("help").onMainButton.apply(<this>, args);
+*/
+
+class MethodsExecuter {
+  exectute(expression, ...args){
+    const  [type, identify, method, ...rest] = expression.split( this.constructor.SEPARATOR );
+    if (rest.length)
+      console.warn( new TypeError("A string of 3 arguments is expected. Rest parameters ignored") );
+
+    const component = this.constructor.supportedComponents[type];
+    const list = typeof component.list === "function" ? component.list(this) : component.list;
+
+    const target = component.getTarget(list, identify);
+    return this.#executer(target, method, ...args);
+  }
+
+  #executer(target, method, ...args){
+    method = `on${ method }`;
+    if (!( method in target))
+      throw new Error(`${ target.constructor.name }, ${ method } is undefined`);
+
+    return target[ method ].apply(target, args);
+  }
+
+  static supportedComponents = {
+    "command": {
+      list: globalThis.commands,
+      getTarget: (list, identify) => list.get(identify)
+    },
+    "event": {
+      list: globalThis.eventsList,
+      getTarget: (list, identify) => list.get(identify)
+    }
+  }
+
+  static SEPARATOR = "_";
+}
+
+export default MethodsExecuter;
