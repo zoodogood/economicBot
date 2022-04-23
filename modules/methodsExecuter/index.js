2bc2d91472cb67124981c70d2fb42c0e767bdadc
zoodogood <zoodogood@gmail.com>
2022-04-23 19:57:08 +0300

    minor fixed's

--- modules/methodsExecuter/index.js
+++ modules/methodsExecuter/index.js
@@ -5,13 +5,14 @@
 */
 
 class MethodsExecuter {
-  exectute(expression, ...args){
+  execute(expression, ...args){
     const  [type, identify, method, ...rest] = expression.split( this.constructor.SEPARATOR );
-    if (rest.length)
+    if (rest.length > 0)
       console.warn( new TypeError("A string of 3 arguments is expected. Rest parameters ignored") );
 
     const component = this.constructor.supportedComponents[type];
-    const list = typeof component.list === "function" ? component.list(this) : component.list;
+    const list = typeof component.list === "function" ?
+      component.list(this) : component.list;
 
     const target = component.getTarget(list, identify);
     return this.#executer(target, method, ...args);
@@ -25,16 +26,21 @@ class MethodsExecuter {
     return target[ method ].apply(target, args);
   }
 
+
   static supportedComponents = {
     "command": {
-      list: globalThis.commands,
+      list: () => globalThis.commands,
       getTarget: (list, identify) => list.get(identify)
     },
     "event": {
-      list: globalThis.eventsList,
+      list: () => globalThis.eventsList,
       getTarget: (list, identify) => list.get(identify)
-    }
-  }
+    },
+    // "site": {
+    //   list: globalThis.site.botAPI,
+    //   getTarget: (list, identify) => null // in developing
+    // }
+  };
 
   static SEPARATOR = "_";
 }
