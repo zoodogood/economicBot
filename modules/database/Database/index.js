56b0775c0ec62b1ae5d58ed42ffe3c1b0590e471
zoodogood <zoodogood@gmail.com>
2022-04-23 19:57:45 +0300

    throw error when database not connected

--- modules/database/Database/index.js
+++ modules/database/Database/index.js
@@ -4,6 +4,26 @@ class Database {
   constructor({ host, user, password, database }){
     this.sql = sql;
     this.pool = this.sql.createPool({ host, user, password, database }).promise();
+
+    this.launched = null;
+  }
+
+
+  get whenPool(){
+    return new Promise(async (resolve, reject) => {
+      const checkConnection = async () => await this.pool.query("select 1");
+      try {
+        await checkConnection();
+        resolve(this);
+      } catch (err) {
+        if (err.message === "connect ECONNREFUSED ::1:3306"){
+          resolve(undefined);
+          return;
+        }
+        return reject(err);
+
+      }
+    });
   }
 }
 
